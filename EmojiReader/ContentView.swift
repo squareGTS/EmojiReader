75d01901fa076f37ddad9af9e8b51c0c80f1b0a4
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-08-19 18:42:53 +0300

    Initial Commit

---
+++ EmojiReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiReader
+//
+//  Created by Maxim Bekmetov on 19.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
